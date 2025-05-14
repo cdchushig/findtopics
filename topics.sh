6b2773e6a0ddfcecb4519191188aff21b9f6dc40
cdchushig <cdavid.chushig@gmail.com>
2025-05-14 14:52:39 +0200

    Added new script

--- topics.sh
+++ topics.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-python src/find_topics.py --language='english' --dataset='firearms' --min_samples=1000 --min_cluster_size=10000 --cluster_selection_epsilon=0.5 --stop_words='en'
-python src/find_topics.py --language='english' --dataset='firearms' --min_samples=1000 --min_cluster_size=10000 --cluster_selection_epsilon=0.2 --stop_words='en'
-python src/find_topics.py --language='english' --dataset='firearms' --min_samples=100 --min_cluster_size=2000 --cluster_selection_epsilon=0.3 --stop_words='en'
-python src/find_topics.py --language='english' --dataset='firearms' --min_samples=100 --min_cluster_size=2000 --cluster_selection_epsilon=0.5 --stop_words='en'
+python src/find_topics.py --language='english' --dataset='firearms' --min_samples=500 --min_cluster_size=1000 --cluster_selection_epsilon=0.5 --stop_words='en' --keyword_list='twitter'
+python src/find_topics.py --language='english' --dataset='firearms' --min_samples=300 --min_cluster_size=1000 --cluster_selection_epsilon=0.2 --stop_words='en' --keyword_list='twitter'
+python src/find_topics.py --language='english' --dataset='firearms' --min_samples=100 --min_cluster_size=2000 --cluster_selection_epsilon=0.3 --stop_words='en' --keyword_list='twitter'
+python src/find_topics.py --language='english' --dataset='firearms' --min_samples=100 --min_cluster_size=2000 --cluster_selection_epsilon=0.5 --stop_words='en' --keyword_list='twitter'
+python src/find_topics.py --language='english' --dataset='firearms' --min_samples=300 --min_cluster_size=3000 --cluster_selection_epsilon=0.5 --stop_words='en' --keyword_list='twitter'
